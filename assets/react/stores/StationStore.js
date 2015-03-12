2c26a1a504715b26427b9664b2f8164185fb14fd
Alex Muro <am3081@gmail.com>
2015-03-12 14:49:20 -0400

    layers

--- assets/react/stores/StationStore.js
+++ assets/react/stores/StationStore.js
@@ -102,7 +102,7 @@ StationStore.dispatchToken = AppDispatcher.register(function(payload) {
   switch(action.type) {
 
     case ActionTypes.RECEIVE_STATIONS:
-      console.log('StationStore / receieve_stations')
+      //console.log('StationStore / receieve_stations')
       _addStations(action.data);
       StationStore.emitChange();
     break;
