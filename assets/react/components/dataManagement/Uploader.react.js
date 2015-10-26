216961f6f9f494440a0d11f055681336349b1624
Ryan <r.k.dubowsky@gmail.com>
2015-10-26 10:48:35 -0400

    Rearranged text in uploader to fix it incorrectly displaying. Added a state property that keeps track of files recieved in this session to give user more immediate feedback

--- assets/react/components/dataManagement/Uploader.react.js
+++ assets/react/components/dataManagement/Uploader.react.js
@@ -17,16 +17,24 @@ var React = require('react'),
 
 var Upload = React.createClass({
    
+    getInitialState: function(){
+        return{
+            recieved:[]            
+        }
+
+    },
+
     uploadStarted: function(err,data){
         console.log('upload started',err,data);
     },
 
     onDrop: function(files){
         var scope = this;
+        var curState = scope.state;
         if(files && files.length > 0){
             
             files.forEach( function (file){
-                
+                curState.recieved.push(file.name);
                 var req = request.post('/upload/'+scope.props.currentAgency.datasource)  
                     .attach('file', file, file.name)
                     .end(scope.uploadStarted)
@@ -35,7 +43,7 @@ var Upload = React.createClass({
                     });
             
             });
-            
+            scope.setState(curState);
         }
     },
     
@@ -43,11 +51,43 @@ var Upload = React.createClass({
         
         console.log('datasource overview',this.props.currentAgency);
 
+        var recieved = "";
+
+        var scope = this;
+
+        if(scope.state.recieved.length == 0){
+            recieved = "None";
+        }
+        else if(scope.state.recieved.length == 1){
+            recieved = scope.state.recieved[0];
+        }
+        else{
+            scope.state.recieved.forEach(function(fileName,index){
+                if(index == scope.state.recieved.length-1){
+                    recieved = recieved + fileName;               
+                }
+                else{
+                    recieved = recieved + fileName + ", ";
+                }
+
+            })            
+        }
+
+
+
+
+
+
+
         return (
             <div>
+                <div style={{fontSize:'12px'}}>Drop Files below or Click to Upload.</div>
                 <Dropzone onDrop={this.onDrop} size="100%">
-                  <div style={{fontSize:'12px'}}>Drop Files here or Click to Upload.</div>
+                    <div style={{fontSize:'10px'}}>Upload Here</div>
                 </Dropzone>
+                <div style={{fontSize:'12px',marginTop:'10px',marginBottom:'10px'}}>
+                    Files recieved: {recieved}
+                </div>
                 <div>
                     <JobList activeJobs={this.props.activeJobs} />
                 </div>
