050bc311d8ee8957db4df58b9602be4aca5d84d1
Alex <am3081@gmail.com>
2015-11-17 11:00:33 -0500

    test 123

--- assets/react/constants/AppConstants.js
+++ assets/react/constants/AppConstants.js
@@ -1,4 +1,4 @@
-
+'use strict';
 
 var keyMirror = require('keymirror');
 
@@ -14,8 +14,7 @@ module.exports = {
     SET_SELECTED_STATE:null,
     SET_SELECTED_STATION:null,
     CBM_INITIALIZED:null,
-    DELETE_USER:null,
-
+    
     // -- Filters
     FILTER_YEAR:null,
     FILTER_MONTH:null,
